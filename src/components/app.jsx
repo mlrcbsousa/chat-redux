f57fa14a44604e38d7595b01ca49575dc3cb28d0
Manuel Sousa <mlrcbsousa@gmail.com>
2019-03-23 20:02:14 +0100

    link to direct svg

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -6,10 +6,12 @@ import MessageList from '../containers/message_list';
 import ChannelList from '../containers/channel_list';
 
 const App = () => {
+  const src = "https://raw.githubusercontent.com/lewagon/chat-redux/master/assets/images/logo.svg";
+
   return (
     <div className="messaging-wrapper">
       <div className="logo-container">
-        <img className="messaging-logo" src="assets/images/logo.svg" alt="logo" />
+        <img className="messaging-logo" src={src} alt="logo" />
       </div>
       <ChannelList />
       <MessageList />
